bf3ac753e8e9fa139b102504579310fe1c1b7b4b
Muhammad Amin <failasuf99@gmail.com>
2023-01-30 21:28:32 +0700

    api init

---
+++ api/routes/users.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+const model = require('../models/index');
+
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+  try {
+    const users = await model.users.findAll();
+    if (users.length !== 0) {
+      res.json({
+        'status': 'OK',
+        'messages': '',
+        'data': users
+      })
+    } else {
+      res.json({
+        'status': 'ERROR',
+        'messages': 'EMPTY',
+        'data': {}
+      })
+    }
+  } catch (err) {
+    res.json({
+      'status': 'ERROR',
+      'messages': err.message,
+      'data': {}
+    })
+  }
+});
+
+module.exports = router;
